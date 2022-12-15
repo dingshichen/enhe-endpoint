4d095a67dbd41e5852dd7356a07c7a38b1e0beca
ding.shichen <foreverhuiqiao@126.com>
2022-12-15 19:07:23 +0800

    适配 IDE 从 212 版本开始

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.enhe"
-version = "0.4"
+version = "0.5"
 
 repositories {
     mavenCentral()
@@ -32,7 +32,7 @@ tasks {
     }
 
     patchPluginXml {
-        sinceBuild.set("211")
+        sinceBuild.set("212")
     }
 
 //    signPlugin {
