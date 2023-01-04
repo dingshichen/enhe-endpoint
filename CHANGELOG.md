7d0175f990a74a3299aefbf70a4cdb07336caccf
ding.shichen <foreverhuiqiao@126.com>
2023-01-04 20:07:25 +0800

    使用 changelog 插件

---
+++ CHANGELOG.md
@@ -0,0 +1,24 @@
+# Changelog
+
+## Unreleased
+
+### Added
+
+### Changed
+
+### Deprecated
+
+### Removed
+
+### Fixed
+
+### Security
+
+## 1.2.1 - 2023-01-04
+
+### Added
+- Entity support navigate to mapper
+- Use changelog Plugin build notes
+
+### Fixed
+- Listener navigate bug fix
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,51 @@
+fun properties(key: String) = project.findProperty(key).toString()
+
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.7.20"
     id("org.jetbrains.intellij") version "1.10.1"
+    id("org.jetbrains.changelog") version "2.0.0"
 }
 
-group = "com.enhe"
-version = "1.2"
+group = properties("pluginGroup")
+version = properties("pluginVersion")
 
 repositories {
     mavenCentral()
 }
 
+kotlin {
+    jvmToolchain(11)
+}
+
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2022.3")
-    type.set("IC") // Target IDE Platform
+    version.set(properties("platformVersion"))
+    type.set(properties("platformType")) // Target IDE Platform
 //    localPath.set("/Users/dingshichen/Library/Application Support/JetBrains/Toolbox/apps/IDEA-U/ch-0/223.7571.182/IntelliJ IDEA.app")
 
     plugins.set(listOf("com.intellij.java"))
 }
 
+changelog {
+    path.set(file("CHANGELOG.md").canonicalPath)
+}
+
 tasks {
-    // Set the JVM compatibility versions
-    withType<JavaCompile> {
-        sourceCompatibility = "11"
-        targetCompatibility = "11"
-    }
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "11"
+    patchPluginXml {
+        sinceBuild.set(properties("pluginSinceBuild"))
     }
 
     patchPluginXml {
-        sinceBuild.set("212")
+        changeNotes.set(provider {
+            with(changelog) {
+                renderItem(
+                    getOrNull(properties("pluginVersion")) ?: getLatest(),
+                    org.jetbrains.changelog.Changelog.OutputType.HTML,
+                )
+            }
+        })
     }
 
 //    signPlugin {
